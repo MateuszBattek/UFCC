707612688788ab44db8f4eb4a44c9e8e0dab2027
Mateusz Battek <mbattek@student.agh.edu.pl>
2025-12-31 13:40:45 +0100

    match update 31/12

--- somanytrash.py
+++ somanytrash.py
@@ -1,7 +1,12 @@
+from datetime import datetime
 import json
 
-with open("teams_geo_info.json", mode="r", encoding="utf-8") as read_file:
+with open("match_list.json", mode="r", encoding="utf-8") as read_file:
     data = json.load(read_file)
 
 
+with open("alt_names.json", mode="r", encoding="utf-8") as read_file:
+    alt_names = json.load(read_file)
+
+date_format = '%d/%m/%Y'
 print(len(data))
